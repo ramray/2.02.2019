d94b3fb55e928035423aaabf26a3f6ffb8fad168
Roman Dyagilev <roman.dyagilev@gmail.com>
2019-12-03 23:30:14 +0300

    Signed-off-by: Roman Dyagilev <roman.dyagilev@gmail.com>

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,6 @@ package ru.geekbrains.java_one.lesson_a.online;
 
 public class HelloWorld {
     public static void main (String[] args) {
-        int a = 2;
-        int b = 3;
-        float c = 4f;
-        int d = 5;
-        System.out.print(a * ( b + (c / d)));
+            System.out.print("HelloWorld");
     }
 }
---
+++ ResolveResault.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.java_one.lesson_a.online;
+
+public class ResolveResault {
+    public static void main (String[] args) {
+        int a = 2;
+        int b = 3;
+        float c = 4f;
+        int d = 5;
+        System.out.print(a * ( b + (c / d)));
+    }
+
+}
