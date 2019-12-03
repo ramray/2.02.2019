21e8c8c9c9e37ffc31e1a626aa99833ed87c9938
Roman Dyagilev <roman.dyagilev@gmail.com>
2019-12-04 01:04:41 +0300

    Signed-off-by: Roman Dyagilev <roman.dyagilev@gmail.com>

--- PositivNum.java
+++ PositivNum.java
@@ -1,4 +1,16 @@
 package ru.geekbrains.java_one.lesson_a.online;
+//Написать метод, которому в качестве параметра передается целое число, метод должен проверить положительное ли число
+// передали, или отрицательное. Замечание: ноль считаем положительным числом. Результат работы метода вывести в консоль
+import java.util.Scanner;
 
 public class PositivNum {
-}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int a = in.nextInt();
+        if (a > 0 || a == 0) {
+            System.out.print("Num a positive");
+        }  else
+            System.out.print("Num a negative");
+        }
+    }
