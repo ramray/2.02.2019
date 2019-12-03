f901e4a6fa7617634086b1d913f72f28d674c9c9
Roman Dyagilev <roman.dyagilev@gmail.com>
2019-12-03 23:31:49 +0300

    Signed-off-by: Roman Dyagilev <roman.dyagilev@gmail.com>

--- ResolveResault.java
+++ ResolveResault.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.java_one.lesson_a.online;
-
+//Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат с плавающей точкой,
+// где a, b, c, d – целочисленные входные параметры этого метода;
 public class ResolveResault {
     public static void main (String[] args) {
         int a = 2;
